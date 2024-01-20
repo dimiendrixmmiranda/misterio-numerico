e166800db4fb5ae0cc4e35e8d0286a00f0666ba6
dimi <dimiendrixmmiranda@gmail.com>
2024-01-20 17:04:27 -0300

    Correção bug do 1 ao 9

--- script/reconhecimentoDeVoz.js
+++ script/reconhecimentoDeVoz.js
@@ -18,6 +18,33 @@ function onSpeak(e){
 }
 
 function exibirChuteNaTela(chute){
+    if(chute == 'número um' || chute == 'número 1'){
+        chute = '1'
+    }
+    if(chute == 'número dois' || chute == 'número 2'){
+        chute = '2'
+    }
+    if(chute == 'número três' || chute == 'número 3'){
+        chute = '3'
+    }
+    if(chute == 'número quatro' || chute == 'número 4'){
+        chute = '4'
+    }
+    if(chute == 'número cinco' || chute == 'número 5'){
+        chute = '5'
+    }
+    if(chute == 'número seis' || chute == 'número 6'){
+        chute = '6'
+    }
+    if(chute == 'número sete' || chute == 'número 7'){
+        chute = '7'
+    }
+    if(chute == 'número oito' || chute == 'número 8'){
+        chute = '8'
+    }
+    if(chute == 'número nove' || chute == 'número 9'){
+        chute = '9'
+    }
     elementoChute.innerHTML = `
         <p>Você disse:</p>
         <div class="valor-falado">${chute}</div>
--- script/validacao.js
+++ script/validacao.js
@@ -4,7 +4,36 @@ const elementoNumeroSecreto = document.querySelector('#numeroSecreto')
 const elementoTentativas = document.querySelector('#tentativas')
 
 function vereficaSeOChuteEValido(chute) {
+    if(chute == 'número um' || chute == 'número 1'){
+        chute = '1'
+    }
+    if(chute == 'número dois' || chute == 'número 2'){
+        chute = '2'
+    }
+    if(chute == 'número três' || chute == 'número 3'){
+        chute = '3'
+    }
+    if(chute == 'número quatro' || chute == 'número 4'){
+        chute = '4'
+    }
+    if(chute == 'número cinco' || chute == 'número 5'){
+        chute = '5'
+    }
+    if(chute == 'número seis' || chute == 'número 6'){
+        chute = '6'
+    }
+    if(chute == 'número sete' || chute == 'número 7'){
+        chute = '7'
+    }
+    if(chute == 'número oito' || chute == 'número 8'){
+        chute = '8'
+    }
+    if(chute == 'número nove' || chute == 'número 9'){
+        chute = '9'
+    }
+    
     const numero = +chute
+    console.log(numero)
     
     if(chute == 'game over'){
         const musicaPerdedor = new Audio('./musicaPerdedor.m4a')
